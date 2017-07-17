4f9c4f3070daae0c51dbc240defeaadbadfdd797
Eugene Lim <limzhiweieugene@gmail.com>
2017-07-18 02:25:42 +0900

    test

--- client/src/containers/calendar.js
+++ client/src/containers/calendar.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import BigCalendar from 'react-big-calendar';
-import moment from 'moment';
 
-import events from './events';
 
 class Calendar extends Component {
   render() {
-    BigCalendar.momentLocalizer(moment);
 
     return (
       <div>
-        <BigCalendar
-          events={events}
-        />
+        Test
       </div>
     );
   }
